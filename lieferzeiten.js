06776d468a18886e3ef3a01cebfc1ce5a26e1c35
ADarkHero <maik.riedlsperger@gmail.com>
2021-12-22 16:43:15 +0100

    Added additional message for not available products.

--- lieferzeiten.js
+++ lieferzeiten.js
@@ -11,9 +11,19 @@ try{
 		var lieferzeitentext_temp = lieferhtml.substring(lieferindex, lieferindex+100); //Alles vor "Die Lieferung erfolgt innerhalb" abschneiden
 		var lieferindex = lieferzeitentext_temp.indexOf("<br>"); //Nach dem <br> suchen, um alles danach abzuschneiden
 		var lieferzeitentext = lieferzeitentext_temp.substring(0, lieferindex); //Vom neuen String alles nach dem <br> abschneiden, dass nur noch der String übrig bleibt, den wir benötigen
-		if (document.querySelector('.is-in-stock') !== null && $( ".is-in-stock" ).first().text().indexOf("Sulingen") >= 0){
+		//Nachricht anzeigen, wenn Produkt nicht verfügbar
+		if (document.querySelector('.is-not-available') !== null && document.querySelector('.is-in-stock') == null){
+			lieferzeitentext = "Derzeit nicht verfügbar. Bestellware! " + lieferzeitentext;
+		}
+		//Lieferzeit vermindern, wenn in Sulingen auf Lager
+		if (document.querySelector('.is-in-stock') !== null && $( ".is-in-stock" ).text().indexOf("Sulingen") >= 0){
 		   lieferzeitentext = lieferzeitentext.replace(/[0-9]+/g, "3"); //Wenn die Ware auf Lager ist, wird die Lieferzeit auf 3 Tage gesetzt
 		} 
+		//Lieferzeit vermindern, wenn in Gießen auf Lager
+		if (document.querySelector('.is-in-stock') !== null && $( ".is-in-stock" ).text().indexOf("Gießen") >= 0){
+		   lieferzeitentext = lieferzeitentext.replace(/[0-9]+/g, "3"); //Wenn die Ware auf Lager ist, wird die Lieferzeit auf 3 Tage gesetzt
+		} 
+		//Lieferzeit anzeigen
 		if(window.location.pathname !== "/"){
 			$( ".nw_delivery-status" ).append( "<p>"+lieferzeitentext+"</p>" ); //Die Lieferzeiten an die Klasse anhängen
 		}
