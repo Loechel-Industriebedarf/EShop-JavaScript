f61d6f757c21041514f7da6ee032d169a3d36e5c
ADarkHero <maik.riedlsperger@gmail.com>
2022-02-21 11:23:58 +0100

    product/view

--- lieferzeiten.js
+++ lieferzeiten.js
@@ -3,15 +3,15 @@ Lieferzeiten Anzeige
 ************************************************************************************************************************************************/
 
 try{
-	//Lieferzeiten nur bei Artikeln anzeigen (haben .html in der URL)
-	if(window.location.href.indexOf("html") >= 0){
+	//Lieferzeiten nur bei Artikeln anzeigen (haben product/view in der URL)
+	if(window.location.href.indexOf("product/view") >= 0){
 		var lieferzeiten = document.getElementsByClassName("legal"); //HTML der Lieferzeiten Klasse auslesen
 		var lieferhtml = lieferzeiten[1]['outerHTML']; //HTML "reduzieren"; Unnötige Informationen des Objekts entfernen
 		var lieferindex = lieferhtml.indexOf("Die Lieferung erfolgt innerhalb"); //Nach "Die Lieferung erfolgt innerhalb" suchen, um alles vor diesem String abzuschneiden
 		var lieferzeitentext_temp = lieferhtml.substring(lieferindex, lieferindex+100); //Alles vor "Die Lieferung erfolgt innerhalb" abschneiden
 		var lieferindex = lieferzeitentext_temp.indexOf("<br>"); //Nach dem <br> suchen, um alles danach abzuschneiden
 		var lieferzeitentext = lieferzeitentext_temp.substring(0, lieferindex); //Vom neuen String alles nach dem <br> abschneiden, dass nur noch der String übrig bleibt, den wir benötigen
-		var lieferzeitentext_full = lieferzeitentext;
+		var lieferzeitentext_full = lieferzeitentext; //Lieferzeitentext ohne verminderte Lieferzeit
 		
 		//Lieferzeit vermindern, wenn in Gießen auf Lager
 		if (document.querySelector('.is-in-stock') !== null && $( ".is-in-stock" ).text().indexOf("Gießen") >= 0){
