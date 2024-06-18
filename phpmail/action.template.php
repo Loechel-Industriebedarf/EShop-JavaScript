ec79524d7e8d27eaf7fda0b565b461e79bd5510c
ADarkHero <maik.riedlsperger@gmail.com>
2024-06-18 16:27:00 +0200

    ...

--- phpmail/action.template.php
+++ phpmail/action.template.php
@@ -14,15 +14,15 @@ require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 
 try {
- 
+
 	
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_OFF;	                        //Enable verbose debug output; DEBUG_SERVER to turn on
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.udag.de';                     	//Set the SMTP server to send through
+    $mail->Host       = 'SMTP SERVER';	                     	//Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'loechel-industriebedarfde-0048';       //SMTP username
-    $mail->Password   = 'Ctu7Yf^c7Rd"+_J';                      //SMTP password
+    $mail->Username   = 'USERNAME';       						//SMTP username
+    $mail->Password   = 'PASSWORD';                      		//SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 	$mail->CharSet = 'UTF-8';									//Fix for umlauts
