8fe7fd039b5fdad4b162b40e3417ac794db60f1c
ADarkHero <maik.riedlsperger@gmail.com>
2024-07-03 08:13:52 +0200

    Added captcha

--- phpmail/retouren.template.php
+++ phpmail/retouren.template.php
@@ -9,30 +9,34 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
+require 'verifyCaptcha.php';
+
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-
-	
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_OFF;	                        //Enable verbose debug output; DEBUG_SERVER to turn on
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'SMTP SERVER';	                     	//Set the SMTP server to send through
+    $mail->Host       = 'SMTP-SERVER';                  	   	//Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'USERNAME';       						//SMTP username
-    $mail->Password   = 'PASSWORD';                      		//SMTP password
+    $mail->Username   = 'USERNAME';						       //SMTP username
+    $mail->Password   = 'PASSWORD';     		                 //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 	$mail->CharSet = 'UTF-8';									//Fix for umlauts
 	$mail->Encoding = PHPMailer::ENCODING_BASE64;				//Fix for umlauts
+	
+	
+	if($_POST["captcha"] == null || $_POST["captcha"] == ""){
+		throw new Exception('Captcha wurde nicht korrekt gelöst!');
+	}
+	else{
+		$captcha = verifyCaptcha($_POST["captcha"]);
+		if($captcha == false){ throw new Exception('Captcha wurde nicht korrekt gelöst!'); }
+	}
 
-    //Recipients
-    $mail->setFrom('retouren@loechel-industriebedarf.de', 'Löchel Industriebedarf - Retouren');
-    $mail->addAddress($_POST["email"], $_POST["full_name"]);     			//Add a recipient - User who filled out form
-    $mail->addAddress('retouren@loechel-industriebedarf.de');    			//Add a recipient - Our mail address
-    $mail->addReplyTo($_POST["email"], $_POST["full_name"]);
 	
     //Attachments	
 	if(isset($_FILES['uploadedFiles'])){
@@ -41,17 +45,15 @@ try {
 			 //Make sure we have a file path
 			  if ($tmpFilePath != ""){
 				//Setup our new file path
-				$newFilePath = "./uploads/" . $_FILES['uploadedFiles']['name'][$i];
+				$newFilePath = "./uploads/" . date('Y-m-d') . "_" . $_FILES['uploadedFiles']['name'][$i];
 
 				//Upload the file into the temp dir
 				if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-					echo "<b>Anhang:</b> " . $_FILES['uploadedFiles']['name'][$i].'</br>'; 
-					move_uploaded_file($_FILES['uploadedFiles']['name'][$i], "./uploads/".$_FILES['uploadedFiles']['name'][$i]);
+					//echo "<b>Anhang:</b> " . $_FILES['uploadedFiles']['name'][$i].'</br>'; 
+					move_uploaded_file($_FILES['uploadedFiles']['name'][$i], "./uploads/".$_FILES['uploadedFiles']['name'][$i]);	
 					$mail->addAttachment($newFilePath);         //Add attachments
 				}
 			}
-			 
-			
 		}
 	}	
 	
@@ -62,7 +64,6 @@ try {
 	"<b>Telefonnummer:</b> " . $_POST["phoneNum"] . "<br>" .
 	"<b>E-Mail:</b> " . $_POST["email"] . "<br>" .
 	"<b>Lieferscheinnummer / Auftragsnummer / Rechnungsnummer:</b> " . $_POST["uniqueNum"] . "<br>" .
-	"<b>Artikelnummer & Menge:</b> " . $_POST["amount"] . " x " . $_POST["artNr"] . "<br>" .
 	"<b>Retouren- / Reklamationsgrund:</b> " . $_POST["retReason"] . "<br>" .
 	"<br>" .
 	"<b>Fehlerbeschreibung:</b> <br>" . $_POST["msg_body"] . "<br>";
@@ -77,13 +78,21 @@ try {
 	}
     $mail->Body    =  $msg;
 	
-    $mail->AltBody =  strip_tags($msg); //No html
+	$mail->AltBody =  strip_tags($msg); //No html
 	
-	echo "<br><b><u>Nachrichteninhalt</u>:</b><br>" . $msg . "<br><br>";
+	//echo $msg;
+	
+	
+    //Recipients
+    $mail->setFrom('retouren@loechel-industriebedarf.de', 'Löchel Industriebedarf - Retouren');
+    $mail->addAddress($_POST["email"], $_POST["full_name"]);     			//Add a recipient - User who filled out form
+    $mail->addAddress('retouren@loechel-industriebedarf.de');    			//Add a recipient - Our mail address
+    $mail->addReplyTo($_POST["email"], $_POST["full_name"]);
+	
+    
 
     $mail->send();
-    echo 'Vielen Dank für Ihre Nachricht. Sie wurde erfolgreich versandt. Wir melden uns zeitnah bei Ihnen!<br>' .
-	'Sie können dieses Fenster nun schließen.';
+    echo 'Vielen Dank für Ihre Nachricht. Sie wurde erfolgreich versandt. Wir melden uns zeitnah bei Ihnen!<br>';
 } catch (Exception $e) {
-    echo "Die Nachricht konnte nicht verschickt werden. Bitte versuchen Sie es in ein paar Minuten erneut. Sollte der Fehler dauerhaft auftreten, senden Sie uns bitte eine Mail mit dem folgenden Failercode:<br>{$mail->ErrorInfo}";
+    echo "FEHLER: Die Nachricht konnte nicht verschickt werden. Bitte versuchen Sie es in ein paar Minuten erneut. Sollte der Fehler dauerhaft auftreten, senden Sie uns bitte eine Mail mit dem folgenden Fehlercode:<br>{$mail->ErrorInfo}<br>{$e->getMessage()}";
 }
--- phpmail/vermietung.template.php
+++ phpmail/vermietung.template.php
@@ -9,30 +9,31 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
+require 'verifyCaptcha.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
- 
-	
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_OFF;	                        //Enable verbose debug output; DEBUG_SERVER to turn on
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'SMTP-SERVER';	                     	//Set the SMTP server to send through
+    $mail->Host       = 'SMTP-SERVER';                     		//Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'USERNAME';       						//SMTP username
-    $mail->Password   = 'PASSWORD';   		                   //SMTP password
+    $mail->Password   = 'PASSWORD';                      		//SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 	$mail->CharSet = 'UTF-8';									//Fix for umlauts
 	$mail->Encoding = PHPMailer::ENCODING_BASE64;				//Fix for umlauts
 
-    //Recipients
-    $mail->setFrom('info@loechel-industriebedarf.de', 'Löchel Industriebedarf - Info');
-    $mail->addAddress($_POST["email"], $_POST["full_name"]);     			//Add a recipient - User who filled out form
-    $mail->addAddress('maik.riedlsperger@loechel-industriebedarf.de');    			//Add a recipient - Our mail address
-    $mail->addReplyTo($_POST["email"], $_POST["full_name"]);
+	if($_POST["captcha"] == null || $_POST["captcha"] == ""){
+		throw new Exception('Captcha wurde nicht korrekt gelöst!');
+	}
+	else{
+		$captcha = verifyCaptcha($_POST["captcha"]);
+		if($captcha == false){ throw new Exception('Captcha wurde nicht korrekt gelöst!'); }
+	}
 	
 	$msg = 
 	"<b>Firma:</b> " . $_POST["firm"] . "<br>" .
@@ -40,8 +41,8 @@ try {
 	"<b>E-Mail:</b> " . $_POST["email"] . "<br>" .
 	"<b>Kundennummer:</b> " . $_POST["custnr"] . "<br>" .
 	"<b>Gewünschtes PSAgA-Set:</b> " . $_POST["setSelect"] . "<br>" .
-	"<b>Mietzeitraum (Start):</b> " . $_POST["startDate"] . "<br>" .
-	"<b>Mietzeitraum (Ende):</b> " . $_POST["endDate"] . "<br>" .
+	"<b>Mietzeitraum:</b> " . $_POST["startDate"] . " - " . $_POST["endDate"] . "<br>" .
+	"<b>Versandart:</b> " . $_POST["shipping"] . "<br>" .
 	"<br>" .
 	"<b>Individuelle Nachricht:</b> <br>" . $_POST["msg_body"] . "<br>";
 
@@ -51,12 +52,17 @@ try {
     $mail->Body    =  $msg;
 	
     $mail->AltBody =  strip_tags($msg); //No html
+	//echo $msg;
 	
-	echo "<br><b><u>Nachrichteninhalt</u>:</b><br>" . $msg . "<br><br>";
-
+	
+    //Recipients
+    $mail->setFrom('info@loechel-industriebedarf.de', 'Löchel Industriebedarf - Info');
+    $mail->addAddress($_POST["email"], $_POST["full_name"]);     			//Add a recipient - User who filled out form
+    $mail->addAddress('maik@loechel-industriebedarf.de');    				//Add a recipient - Our mail address
+    $mail->addReplyTo($_POST["email"], $_POST["full_name"]);
+	
     $mail->send();
-    echo 'Vielen Dank für Ihre Nachricht. Sie wurde erfolgreich versandt. Wir melden uns zeitnah bei Ihnen!<br>' .
-	'Sie können dieses Fenster nun schließen.';
+    echo 'Vielen Dank für Ihre Nachricht. Sie wurde erfolgreich versandt. Wir melden uns zeitnah bei Ihnen!<br>';
 } catch (Exception $e) {
-    echo "Die Nachricht konnte nicht verschickt werden. Bitte versuchen Sie es in ein paar Minuten erneut. Sollte der Fehler dauerhaft auftreten, senden Sie uns bitte eine Mail mit dem folgenden Failercode:<br>{$mail->ErrorInfo}";
+    echo "FEHLER: Die Nachricht konnte nicht verschickt werden. Bitte versuchen Sie es in ein paar Minuten erneut. Sollte der Fehler dauerhaft auftreten, senden Sie uns bitte eine Mail mit dem folgenden Fehlercode:<br>{$mail->ErrorInfo}<br>{$e->getMessage()}";
 }
